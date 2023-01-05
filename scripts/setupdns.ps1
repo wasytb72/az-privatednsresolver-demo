44bd4453e85448afa0734ccc7ca4f9cec7091247
David Wahby <wasytb@gmail.com>
2023-01-05 14:13:08 +0100

    Post-Deployment dns script added

---
+++ scripts/setupdns.ps1
@@ -0,0 +1,9 @@
+#Run on the vm-dns
+#Create conditional forwarder againt the Resolver DNS Inbound Endpoint
+Add-DnsServerConditionalForwarderZone -name "test.contoso.com" -MasterServers 10.0.0.68
+
+#Create authoritative zone contoso.com
+Add-DnsServerPrimaryZone -Name "contoso.com"
+
+#create record A probedns.contoso.com with IP 10.3.0.4
+Add-DnsServerResourceRecordA -Name probedns1 -IPv4Address 10.3.0.5 -ZoneName contoso.com
